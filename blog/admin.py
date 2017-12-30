de89339c7e5bc2ba798b4e1c849b6d6d160bd12d
Administrator <117220100@qq.com>
2017-12-31 00:11:54 +0800

    Second

---
+++ blog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Post, Comment
+# Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}  # 预填充
+    raw_id_fields = ('author', )  # author 字段 搜索控件
+    date_hierarchy = 'publish'  # 搜索框下日期快速导航
+    ordering = ['status', 'publish']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
